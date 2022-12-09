83e848ae34f346e6cc99aaec1eb49ee37d8ccf4c
notEnoughTime <108775412+42273@users.noreply.github.com>
2022-12-09 09:09:35 +0900

    주석 삭제, 코드 정렬

--- server/app.js
+++ server/app.js
@@ -1,25 +1,23 @@
 /* cors mongoose express morgan dotenv multer */
 
-const express = require("express")
+const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const mongoose = require('mongoose');
-const morgan = require('morgan');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
 // app.use(express.urlencoded({extended:true}));
 const reviewRouter = require("./router/reviewRouter");
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI,{dbName:"tour"});
+mongoose.connect(process.env.MONGODB_URI, { dbName: "tour" });
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
+app.use("/api/tour", reviewRouter);
 
-app.use("/api/tour",reviewRouter);
-
-
-app.listen(8070,()=>{
-    console.log('[SERVER]☆ START!☆')
-})
+app.listen(8070, () => {
+    console.log("[SERVER]☆ START!☆");
+});
